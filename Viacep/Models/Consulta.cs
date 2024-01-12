3a98ebb2206e22bbe1afd7e405949a7f3dd45413
LuisFernandoPBPereira <luisfernandopbp@gmail.com>
2024-01-11 23:05:35 -0300

    Função ConsultaSemCEP concluída

--- Viacep/Models/Consulta.cs
+++ Viacep/Models/Consulta.cs
@@ -9,7 +9,7 @@ namespace Viacep.Models
 {
     public class Consulta
     {
-        public void ConsultaCEP(dynamic json)
+        public void ConsultaCEP(string json)
         {
             var novaConsulta = JsonConvert.DeserializeObject<Endereco>(json);
 
@@ -25,6 +25,37 @@ namespace Viacep.Models
             Console.WriteLine($"DDD: {novaConsulta.Ddd}");
             Console.WriteLine($"SIAFI: {novaConsulta.Siafi}");
             Console.WriteLine("\n");
+        }
+
+        public void ConsultaSemCEP(string json)
+        {
+            var list = new List<Endereco>();
+            list = JsonConvert.DeserializeObject<List<Endereco>>(json);
+
+            if(list.Count() == 0)
+            {
+                Console.WriteLine("\nEndereço não encontrado.\n");
+            }
+            else
+            {
+                Console.WriteLine("\nEste foi o resultado da consulta:\n");
+
+                foreach(var item in list)
+                {
+                    Console.WriteLine("\n");
+                    Console.WriteLine($"CEP: {item.Cep}");
+                    Console.WriteLine($"Rua: {item.Logradouro}");
+                    Console.WriteLine($"Complemento: {item.Complemento}");
+                    Console.WriteLine($"Bairro: {item.Bairro}");
+                    Console.WriteLine($"Localidade: {item.Localidade}");
+                    Console.WriteLine($"UF: {item.Uf}");
+                    Console.WriteLine($"IBGE: {item.Ibge}");
+                    Console.WriteLine($"GIA: {item.Gia}");
+                    Console.WriteLine($"DDD: {item.Ddd}");
+                    Console.WriteLine($"SIAFI: {item.Siafi}");
+                    Console.WriteLine("\n");
+                }
+            }
         }
     }
 }
